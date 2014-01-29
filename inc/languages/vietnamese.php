57bd66298797f30cc1c9c78f8899b3886874ca3d
Giang Manh <manh@nadia.bz>
2014-01-29 15:58:02 +0900

    Init

---
+++ inc/languages/vietnamese.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * MyBB 1.6 Vietnamese Language Pack
+ * Translated by Giang Mạnh 2014
+ */
+
+// The friendly name of the language
+$langinfo['name'] = "Tiếng Việt";
+
+// The author of the language
+$langinfo['author'] = "Giang Mạnh";
+
+// The language authors website
+$langinfo['website'] = "";
+
+// Compatible version of MyBB
+$langinfo['version'] = "1600";
+
+// Sets if the translation includes the Admin CP (1 = yes, 0 = no)
+$langinfo['admin'] = 0;
+
+// Sets if the language is RTL (Right to Left) (1 = yes, 0 = no)
+$langinfo['rtl'] = 0;
+
+// Sets the lang in the <html> on all pages
+$langinfo['htmllang'] = "vi";
+
+// Sets the character set, blank uses the default.
+$langinfo['charset'] = "UTF-8";
+?>
