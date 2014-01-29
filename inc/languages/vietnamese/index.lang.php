d4ff1e888f07cd826bb504e3b6e03ac065c98aa2
Giang Manh <manh@nadia.bz>
2014-01-29 17:11:49 +0900

    Chung và người dùng

---
+++ inc/languages/vietnamese/index.lang.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * MyBB 1.6 Vietnamese Language Pack
+ * By Giang Mạnh 2014
+ */
+
+$l['boardstats'] = "Thống kê";
+$l['new_posts'] = "Diễn đàn có bài mới";
+$l['no_new_posts'] = "Diễn đàn không có bài mới";
+$l['forum_locked'] = "Diễn đàn đã khoá";
+$l['forum_unapproved_posts_count'] = "Còn {1} bài chưa được duyệt trong diễn đàn này.";
+$l['forum_unapproved_post_count'] = "Còn 1 bài chưa được duyệt trong diễn đàn này";
+$l['forum_unapproved_threads_count'] = "Còn {1} chủ đề chưa được duyệt trong diễn đàn này.";
+$l['forum_unapproved_thread_count'] = "Còn {1} chủ đề chưa được duyệt trong diễn đàn này";
+$l['markread'] = "Đánh dấu đã đọc hết tất cả";
+$l['forumteam'] = "Đội xây dựng diễn đàn";
+$l['forumstats'] = "Thông kê diễn đàn";
+$l['todays_birthdays'] = "Sinh nhật hôm nay";
+$l['birthdayhidden'] = "Ẩn";
+$l['quick_login'] = "Đăng nhập nhanh:";
+$l['index_logout'] = "Thoát ra";
+$l['private_messages'] = "Tin nhắn (PM)";
+$l['pms_new'] = "Bạn có {1} tin nhắn mới.";
+$l['pms_unread_total'] = "(Bạn có {1} tin nhắn chưa đọc trong tổng số {2} tin nhắn.)";
+$l['stats_posts_threads'] = "Our members have made a total of {1} posts in {2} threads.";
+$l['stats_numusers'] = "We currently have {1} members registered.";
+$l['stats_newestuser'] = "Thành viên mới nhất, <b>{1}</b>";
+$l['stats_mostonline'] = "The most users online at one time was {1} on {2} at {3}";
+$l['whos_online'] = "Có ai đang online";
+$l['complete_list'] = "Complete List";
+$l['online_online_plural'] = "người dùng";
+$l['online_online_singular'] = "người dùng";
+$l['online_member_plural'] = "thành viên";
+$l['online_member_singular'] = "thành viên";
+$l['online_anon_plural'] = "là";
+$l['online_anon_singular'] = "là";
+$l['online_guest_plural'] = "khách";
+$l['online_guest_singular'] = "khách";
+$l['online_note'] = "{1} {2} đã online trong vòng {3} phút qua ({4} {5}, {6} trong số {7} ẩn danh, và {8} {9}).";
+$l['subforums'] = "<strong>Diễn đàn nhỏ:</strong>";
+?>
