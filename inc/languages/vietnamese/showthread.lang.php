51d32655c3bc621b263f743844ddfe531f098c69
Giang Manh <manh@nadia.bz>
2014-01-29 17:41:16 +0900

    Bài viết

---
+++ inc/languages/vietnamese/showthread.lang.php
@@ -0,0 +1,93 @@
+<?php
+/**
+ * MyBB 1.6 English Language Pack
+ * Copyright 2010 MyBB Group, All Rights Reserved
+ * 
+ * $Id: showthread.lang.php 5828 2012-05-08 16:06:16Z Tomm $
+ */
+
+$l['delete_poll'] = "Xoá bầu chọn";
+$l['close_thread'] = "Đóng băng";
+$l['stick_thread'] = "Đính lên đầu";
+
+$l['author'] = "Tác giả";
+$l['message'] = "Nội dung";
+$l['threaded'] = "Chế độ phân tầng";
+$l['linear'] = "Chế độ thời gian";
+$l['next_oldest'] = "Next Oldest";
+$l['next_newest'] = "Next Newest";
+$l['view_printable'] = "Xem bản để in ấn";
+$l['send_thread'] = "Gửi cho bạn bè";
+$l['subscribe_thread'] = "Theo dõi chủ đề này";
+$l['unsubscribe_thread'] = "Bỏ theo dõi";
+$l['moderation_options'] = "Chế độ duyệt:";
+$l['delayed_moderation'] = "Trì hoãn duyệt";
+$l['thread_notes'] = "Xem sửa các ghi chú";
+$l['open_close_thread'] = "Mở / Đóng chủ đề";
+$l['approve_thread'] = "Duyệt chủ đề";
+$l['unapprove_thread'] = "Không phê duyệt";
+$l['delete_thread'] = "Xoá chủ đề";
+$l['delete_posts'] = "Xoá chọn lọc các bài viết";
+$l['move_thread'] = "Di chuyển / sao chép chủ đề";
+$l['stick_unstick_thread'] = "Đính lên đầu / gỡ bỏ";
+$l['split_thread'] = "Chia tách chủ đề";
+$l['merge_threads'] = "Gọp các chủ đề";
+$l['merge_posts'] = "Gộp các bài viết";
+$l['remove_redirects'] = "Remove Redirects";
+$l['remove_subscriptions'] = "Bỏ tất cả các theo dõi";
+$l['poll'] = "Bình chọn:";
+$l['show_results'] = "Hiện kết quả";
+$l['edit_poll'] = "Sửa chữa";
+$l['public_note'] = "<b>Chú ý:</b>Đây là bình chọn công khai. Mọi người sẽ biết bạn bình chọn cho cái nào.";
+$l['total'] = "Tổng số";
+$l['vote'] = "Bầu chọn!";
+$l['total_votes'] = "{1} bầu chọn";
+$l['you_voted'] = "* Bạn đã chọn!";
+$l['poll_closed'] = "Bầu chọn đã đóng.";
+$l['already_voted'] = "Bạn đã từng bầu chọn!";
+$l['undo_vote'] = "Rút phiếu bầu";
+$l['quick_reply'] = "Trả lời nhanh";
+$l['message_note'] = "Gõ trả lời của bạn ở đây.";
+$l['signature'] = "Chữ ký";
+$l['email_notify'] = "Thông báo qua email";
+$l['disable_smilies'] = "Không hiện biểu tượng khuôn mặt";
+$l['post_reply'] = "Đăng trả lời";
+$l['post_reply_img'] = "Trả lời";
+$l['post_thread'] = "Đăng chủ đề";
+$l['preview_post'] = "Xem thử bài viết";
+$l['rating_average'] = "{1} phiếu bầu - {2} trung bình";
+$l['rate_thread'] = "Xếp hạng chủ đề này:";
+$l['thread_rating'] = "Độ hay chủ đề:";
+$l['similar_threads'] = "Các chủ đề có thể hay ho...";
+$l['thread'] = "Chủ đề:";
+$l['replies'] = "Trả lời:";
+$l['views'] = "Lượt xem:";
+$l['lastpost'] = "Nội dung mới";
+$l['messages_in_thread'] = "Nội dung chủ đề";
+$l['users_browsing_thread'] = "Số người đang cùng xem chủ đề này:";
+$l['users_browsing_thread_guests'] = "{1} khách";
+$l['users_browsing_thread_invis'] = "{1} người dùng ẩn";
+$l['users_browsing_thread_reading'] = "đang đọc...";
+$l['inline_delete_posts'] = "Xoá các bài";
+$l['inline_merge_posts'] = "Gộp cách bài";
+$l['inline_split_posts'] = "Chia tách các bài";
+$l['inline_approve_posts'] = "Duyệt các bài";
+$l['inline_unapprove_posts'] = "Không phê duyệt";
+$l['inline_post_moderation'] = "Phê duyệt nhanh:";
+$l['inline_go'] = "Chuyển đến";
+$l['clear'] = "Dọn dẹp";
+$l['thread_closed'] = "Chủ đề đã đóng băng";
+$l['no_subject'] = "Không tiêu đề";
+$l['error_nonextnewest'] = "There are no threads that are newer than the one you were previously viewing.";
+$l['error_nonextoldest'] = "There are no threads that are older than the one you were previously viewing.";
+$l['quickreply_multiquote_selected'] = "You have selected one or more posts to quote.";
+$l['quickreply_multiquote_now'] = "Trích dẫn";
+$l['or'] ="or";
+$l['quickreply_multiquote_deselect'] = "bỏ chọn";
+$l['search_thread'] = "Tìm kiếm trong chủ đề";
+$l['enter_keywords'] = "Thử nhập một vài từ khoá";
+$l['image_verification'] = "Xác nhận hình ảnh";
+$l['verification_note'] = "Xin hãy gõ từ trong hình (cẩn để hệ thống tránh các tấn công từ hackers).";
+$l['verification_subnote'] = "(case insensitive)";
+
+?>
